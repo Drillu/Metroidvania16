46356729ba4662379b80b7e5b8ce93bb92a9890e
Drillu <maxmax98765@yahoo.ro>
2022-06-09 11:13:25 +0300

    garbage menu

---
+++ Metroidvania/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}

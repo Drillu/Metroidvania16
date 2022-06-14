f7de1444e7814fd64dfe50179db9e531ab7f04d4
Drillu <maxmax98765@yahoo.ro>
2022-06-14 22:50:42 +0300

    water gun concept

--- Metroidvania/Assets/Scripts/Player/WaterGun.cs
+++ Metroidvania/Assets/Scripts/Player/WaterGun.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class WaterGun : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 mousePos;
+
+    [SerializeField] private Transform gunCenter;
+    [SerializeField] private Animator gunAnimator;
+    
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            gunAnimator.SetBool("StillShooting", true);
+        }
+        else
+        {
+            gunAnimator.SetBool("StillShooting", false);
+        }
     }
+
+    void FixedUpdate()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg - 180f;
+        gunCenter.rotation = Quaternion.Euler(0f,0f,angle);
+    }
+
 }

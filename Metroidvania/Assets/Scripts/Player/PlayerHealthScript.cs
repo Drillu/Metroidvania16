c9d9bbef9a3359e929c9857a0af5212ce9818693
Drillu <maxmax98765@yahoo.ro>
2022-09-22 20:01:43 +0300

    some health modifications

--- Metroidvania/Assets/Scripts/Player/PlayerHealthScript.cs
+++ Metroidvania/Assets/Scripts/Player/PlayerHealthScript.cs
@@ -5,29 +5,25 @@ using UnityEngine.UI;
 
 public class PlayerHealthScript : MonoBehaviour
 {
-    
+    public Image healthBarImage;
     public LayerMask enemiesLayerMask;
-    private float maxHealth = 5f;
-    private float currHealth;
+    public List<Sprite> healthStates;
+    private int maxHealth = 5;
+    private int currHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-       
         currHealth = maxHealth;
-    }
-
-    private void Update()
-    {
-        
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
        if (GeneralManager.CollisionIsTouchingLayer(other,enemiesLayerMask))
         {
-            Debug.Log("bruh");
-            
+            Debug.Log("Player Takes damage");
+            currHealth--;
+            healthBarImage.sprite = healthStates[currHealth]; //Error right now cuz the game doesn't end
         }
     }
 }

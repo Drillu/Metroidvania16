3d7321cddb80e6576926ee8992eeb0d97acaf98e
RealMisterPotato <realmisterpotato@gmail.com>
2023-02-02 00:02:33 +0200

    A new watergun script

--- Metroidvania/Assets/Scripts/Player/WaterGun.cs
+++ Metroidvania/Assets/Scripts/Player/WaterGun.cs
@@ -5,31 +5,45 @@ public class WaterGun : MonoBehaviour
 {
     private Vector3 mousePos;
 
+    // SerializeField
+    [SerializeField] private GameObject player;
     [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Transform gunCenter;
-    [SerializeField] private Transform firePoint;
     [SerializeField] private SpriteRenderer gunSprite;
     [SerializeField] private float fireRate = 15f;
     Vector2 lookDir;
     Vector2 shotDir;
 
+    private float radius = 8.0f;
+
+    void Start()
+    {
+
+    }
     private void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
        
-        if (gunCenter.rotation.eulerAngles.z > 90f && gunCenter.rotation.eulerAngles.z < 270f)
-        {
-            gunCenter.localScale = new Vector3(1, -1, 1);
+        // position
+        if (player != null){
+            this.transform.position = player.transform.position;
         }
-        else
-        {
-            gunCenter.localScale = new Vector3(1, 1, 1);
+
+        this.transform.position = player.transform.position;
+        // place watergun on a cirlce around the player
+        
+
+        // rotation
+        if (this.transform.rotation.eulerAngles.z > 90f && this.transform.rotation.eulerAngles.z < 270f) {
+            this.transform.localScale = new Vector3(1, -1, 1);
+        } else {
+            this.transform.localScale = new Vector3(1, 1, 1);
         }
             
      
 
         if(Input.GetMouseButton(0)) //Hold
         {
+            // Keep shooting synchronized accourding to the fire
             if (fireRate <= 0)
             {
                 fireRate = 15f;
@@ -44,15 +58,15 @@ public class WaterGun : MonoBehaviour
 
     void FixedUpdate()
     {
-        lookDir = mousePos - gunCenter.position;
-        shotDir = firePoint.position - gunCenter.position;
+        lookDir = mousePos - this.transform.position;
+        shotDir = this.transform.position - this.transform.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 180f;
-        gunCenter.rotation = Quaternion.Euler(0f,0f,angle);
+        this.transform.rotation = Quaternion.Euler(0f,0f,angle);
     }
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(shotDir * 4f, ForceMode2D.Impulse);
     }
 
